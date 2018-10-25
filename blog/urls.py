0c640a8d6aef6e0c1f693dc3c7d939ff15bfdc39
mybestnickname <innotimeagh@Gmail.com>
2018-10-25 13:56:02 +0300

    added login and logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 ]
