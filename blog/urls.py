cb3d83514f58a6fd059a4f40ebd5722a7264dc96
mybestnickname <innotimeagh@Gmail.com>
2018-10-25 16:50:55 +0300

    add comments

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(redirect_field_name = 'registration/logged_out.html'), name='logout'),
+    url(r'^accounts/logout/$',
+        auth_views.LogoutView.as_view(redirect_field_name='registration/logged_out.html'),
+        name='logout'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post,
+        name='add_comment_to_post'),
 ]
