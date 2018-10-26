fc04579ad9210360f803e354ecd14e5f1a44174d
mybestnickname <innotimeagh@Gmail.com>
2018-10-26 09:54:56 +0300

    created approved comments

--- blog/views.py
+++ blog/views.py
@@ -65,3 +65,15 @@ def add_comment_to_post(request, pk):
         new_comment = Comment(author=request.user)
         form = CommentForm(instance=new_comment)
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
